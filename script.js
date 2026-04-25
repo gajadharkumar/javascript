81fac53d9e203a3b2fba458404fb8842ac779d99
gajadharkumar <kr2285763@gmail.com>
2026-04-25 12:31:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// ji se kuch ho sakta hai o keyword hota hai 
+// variable var let const
+// var a=12;
+// let a=12;
+// const=21;
+// let a=12;
+// console.log(a)
+
+
+
+// data types
+// primitive copy karne pe real copy mil jaye string number boolen null bigint
+// reference copy karne pe real copy nahi mile reference mile [] {}() copy nahi kar sakte 
+// null aap ne jan ke koi vale nahi di
+// undefiner aap ne koi variable banaye koi vale nahi di default milti hai
+// symbol->unique immutable value
+// let a= 90071112547n; bigint 
+// let a=[1,2,3];
+// console.log(a)
+// dynamic typing -> js mai statc typing nahi hoti hai isme data change kar skate hai 
+// typeof pata karne ke liye use karte hai uska type kiya hai
