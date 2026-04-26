e7bb4461aa3b862601d854331a13e4544605b4a2
gajadharkumar <kr2285763@gmail.com>
2026-04-26 14:56:52 +0530

    Add comments and structure for grading and rock-paper-scissors functions

--- script.js
+++ script.js
@@ -19,3 +19,67 @@
 // console.log(a)
 // dynamic typing -> js mai statc typing nahi hoti hai isme data change kar skate hai 
 // typeof pata karne ke liye use karte hai uska type kiya hai
+// ||operator
+// Arithmetic compresion logical assignment unary tanary - + / * %
+// let a=12 (= assin the value )
+// 12==12 compresion
+// 12!=13
+// === strict 
+// 12>13? console.log("true") :console.log("false");
+// control flow 
+// if else elseif
+// switch case 
+
+
+// write a function getgrade(score) that:
+//  take a student 's marks (0-100)
+// return the grade base on this logic 
+// 90-100 A+
+// 80-89 A
+// 70-79 B 
+// 60-69 C 
+// 33-59 D 
+// 0-32 fail 
+// function getGrade(score){
+//     if(score >=90 && <=100){
+//         return "A";
+//     }
+//     else if(score>=80&&score<=89){
+//         return "B";
+//     }
+//     else if(score>=70&&score<=69){
+//         return "c";
+//     }
+//     else if(score>=60&&score<=89){
+//         return "D";
+//     }
+//     else if(score>=80&&score<=89){
+//         return "Fail";
+//     }else{
+//         console.log("invaild score")
+//     }
+// }
+// console.log(getGrade(70));
+
+
+// ROCK PAPER SCISSOR LOGIC
+// function rps(user,computer){
+//     if(user===computer) return "draw";
+//     if(user==="rock"&&computer==="scissor") return "user";
+//     if(user==="scissor"&&computer==="paper") return "user";
+//      if(user==="paperr"&&computer==="rock") return "user";
+//      return "computer";
+    
+ 
+    
+
+// }
+//  console.log(rps("rock","scissor")) ;
+// rps("rock","paper");
+
+
+// loop repate karne ke liye loop use karte hai
+//  for while do-while forin foreach
+
+
+
