8cc8d3a0f2c152f4adccad2e6e0bc993f40cf7fd
gajadharkumar <kr2285763@gmail.com>
2026-04-28 21:56:14 +0530

    Add initial HTML and JavaScript files with basic structure and comments dom basic

---
+++ Dom/script.js
@@ -0,0 +1,24 @@
+
+// Dom 
+// html se element select karna 
+// text badlna 
+// html badalna  
+// css badlna 
+//  event listeners
+
+//  let abcd=document.getElementsByClassName("abcd")
+//  console.log(abcd);
+// document.querySelector(h1)
+// let h1 =document.querySelector("h1")
+// // h1.innerText=" hello harsh kaise ho"
+//  h1.textContent=" hello harsh kiya kar rahe  ho"
+
+// let a= document.querySelector("a")
+// // a.href="https://www.google.com"
+// a.setAttribute("href","https://www.google.com")
+  
+
+// let img=document.querySelector("img")
+// img.setAttribute("src","https://images.unsplash.com/photo-1773332585749-5146862ba746?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8")
+
+
