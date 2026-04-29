12695f0a96b3cb04b2eee2b76ab4d93025adf143
gajadharkumar <kr2285763@gmail.com>
2026-04-29 15:01:05 +0530

    Refactor HTML structure and update JavaScript comments for clarity

--- Dom/script.js
+++ Dom/script.js
@@ -22,3 +22,21 @@
 // img.setAttribute("src","https://images.unsplash.com/photo-1773332585749-5146862ba746?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDF8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8")
 
 
+// dynamic dom manipulation
+// let h1 = document.createElement("h1")
+// h1.textContent="hello harsh";
+// document.body.append(h1)
+
+// js se style change karna
+// let h1 = document.querySelector("h1")
+// h1.style.color="red"
+// h1.style.backgroundColor="yellow"
+
+//  class lagna class hatna 
+// let h1 = document.querySelector("h1")
+// h1.classList.add("hulu")
+// remove krne ke liye
+// let h1 = document.querySelector("h1")
+// h1.classList.toggle("hulu") toggle-> class laga hoga to hata dega nahi laga hoga to laga dega
+
+
