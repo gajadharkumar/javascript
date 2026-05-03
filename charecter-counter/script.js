7f89f76e866fe570ff83996d761d0b0ee5b0e184
gajadharkumar <kr2285763@gmail.com>
2026-05-04 02:02:43 +0530

    some changes

--- charecter-counter/script.js
+++ charecter-counter/script.js
@@ -2,6 +2,12 @@
 let inp= document.querySelector("input");
 let span= document.querySelector("span");
 inp.addEventListener("input", function(dets){
-    span .textContent= dets.target.value.length;
+    let left= 20-inp.value.length;
+    span.textContent= left;
+    if(left<0){
+        span.style.color= "red";
+    }else{
+        span.style.color= "white";
+    }
 
 })
