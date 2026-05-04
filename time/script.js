02efe251e43482bf5fbf5241a29ff2b70081f9d7
gajadharkumar <kr2285763@gmail.com>
2026-05-05 02:18:15 +0530

    Comment out unused CSS and JavaScript code for future reference count

--- time/script.js
+++ time/script.js
@@ -1,5 +1,17 @@
 
-let nav=document.querySelector(".nav nav");
-nav.addEventListener("click",function(e){
-    alert("You have clicked the button");
-});
+// let nav=document.querySelector(".nav nav");
+// nav.addEventListener("click",function(e){
+//     alert("You have clicked the button");
+// });
+
+let count=10;
+let intervel = setInterval(function(){
+    if(count>=1){
+        count--;
+        console.log(count);
+
+        }
+        else{
+            clearInterval(intervel);
+        }
+},1000);
